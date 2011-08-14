e304e112e8e337b6c348f8e06ed13b132ef067f5
Shane Becker <veganstraightedge@gmail.com>
2011-08-14 13:20:45 -0700

    config readers and writers

--- lib/commands/settings.rb
+++ lib/commands/settings.rb
@@ -2,48 +2,52 @@ class Settings
   class << self
     def default
       puts "
-Config commands:
-  author
-  email
-  title
-  subtitle
-  source
-"
+      Config commands:
+      author
+      email
+      title
+      subtitle
+      source
+      "
     end
 
     def author(value=:author)
-      if value.is_a?(Symbol)
-        puts "author"
-      else
-      end
+      read_or_write(:author, value)
     end
 
     def email(value=:email)
-      if value.is_a?(Symbol)
-        puts "email"
-      else
-      end
+      read_or_write(:email, value)
     end
 
     def source(value=:source)
-      if value.is_a?(Symbol)
-        puts "source"
-      else
-      end
+      read_or_write(:source, value)
     end
 
     def title(value=:title)
-      if value.is_a?(Symbol)
-        puts "title"
-      else
-      end
+      read_or_write(:title, value)
     end
 
     def subtitle(value=:subtitle)
+      read_or_write(:subtitle, value)
+    end
+
+
+    private
+
+    def read_or_write(property, value=:read_or_write_value)
       if value.is_a?(Symbol)
-        puts "subtitle"
+        read(property)
       else
+        write(property, value)
       end
+    end
+
+    def read(property)
+      puts "config.yml read property"
+    end
+
+    def write(property, value)
+      puts "config.yml write property : #{value}"
     end
   end
 end
